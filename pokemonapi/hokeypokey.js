d026e6a21e0512e074acfa2d9908ed7201d6d44d
Roxanna Coldiron <roxanna@coderedcorp.com>
2020-09-26 16:16:34 -0400

    added css and jquery to make pokemon sprites appear, added border and background to the sprites

---
+++ pokemonapi/hokeypokey.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    
+    for(var i=1; i<=151; i++){
+        $('main').append('<img src="http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + i + '.png">');
+    };
+       
+});
